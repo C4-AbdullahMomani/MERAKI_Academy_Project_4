c407e214493de0b4baa7675abb8f7bec9f337efa
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-11 01:07:08 +0200

    adding pre post function to hashing the user password

--- backend/database/models/userSchema.js
+++ backend/database/models/userSchema.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema(
-  {
-    firstName: { type: String,required: true },
-    lastName: { type: String ,required: true},
-    age: { type: Number },
-    country: { type: String },
-    image: { type: String },
-    followers: [{ type: String }],
-    following: [{ type: String }],
-    email: { type: String, required: true, unique: true },
-    passWord: { type: String, required: true },
-    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
-  },
-  { timestamps: true }
-);
-
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number },
+  country: { type: String },
+  image: { type: String },
+  followers: [{ type: String }],
+  following: [{ type: String }],
+  email: { type: String, required: true, unique: true },
+  passWord: { type: String, required: true },
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+});
+userSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  this.passWord = await bcrypt.hash(this.passWord, 10);
+});
 module.exports = mongoose.model("User", userSchema);
