e871d829a6028d8eb79e9d83abcf54080c3213a2
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-11 01:51:37 +0200

    create authentication middleware

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 usersRouter = express.Router();
 
 const {createNewUser,getUserByFirstName} = require("../controllers/users");
+const authentication=require("../middleware/authentication")
 usersRouter.post("/", createNewUser);
-usersRouter.get("/:firstName",getUserByFirstName)
+usersRouter.get("/:firstName",authentication,getUserByFirstName)
 module.exports = usersRouter;
