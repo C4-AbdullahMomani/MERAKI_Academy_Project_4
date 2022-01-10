c183ecf038aa16f944d3333064ea096056ecd31c
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-10 22:22:12 +0200

    removed timeStamps from userSchema

--- backend/database/models/userSchema.js
+++ backend/database/models/userSchema.js
@@ -11,8 +11,8 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
-  },
-  { timestamps: true }
+  }
+  
 );
 
 module.exports = mongoose.model("User", userSchema);
