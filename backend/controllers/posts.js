e1cb30ba37a295d6e8320bcd969e4b1f1b4bcc50
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-11 02:52:54 +0200

    create getAllPosts Function

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,21 +1,61 @@
+const postsSchema = require("../database/models/postSchema");
+const userModel = require("../database/models/userSchema");
 
-const postsSchema=require("../database/models/postSchema")
-const createNewPost=(req,res)=>{
-const {author,description,image,video,comments}=req.body
-const newPost=new postsSchema({author,description,image,video,comments})
-newPost.save().then((post)=>{
-    res.status(201).json({
+// this function to create new post
+const createNewPost = (req, res) => {
+  const { author, description, image, video, comments } = req.body;
+  const newPost = new postsSchema({
+    author,
+    description,
+    image,
+    video,
+    comments,
+  });
+  newPost
+    .save()
+    .then((post) => {
+      res.status(201).json({
         success: true,
         message: `Post Created successfully`,
         post: post,
+      });
     })
-}).catch((err) => {
-    res.status(500).json({
-      success: false,
-      message: `Server Error`,
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
     });
-  });
+};
 
-}
+//this function to get all post
+const getAllPosts = (req, res) => {
+  // const userId = req.token.userId;
+  // userId: userId,
+  postsSchema
+    .find({})
+    .populate("author")
+    .then((posts) => {
+      if (posts.length) {
+        res.status(200).json({
+          success: true,
+          message: `All the posts`,
 
-module.exports = {createNewPost};
+          posts: posts,
+          comments: posts.comments,
+        });
+      } else {
+        res.status(200).json({
+          success: false,
+          message: `No Posts Yet`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    });
+};
+module.exports = { createNewPost, getAllPosts };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 postRouter = express.Router();
-const { createNewPost} = require("../controllers/posts");
+const { createNewPost,getAllPosts} = require("../controllers/posts");
 postRouter.post("/", createNewPost)
+postRouter.get("/",getAllPosts)
 module.exports = postRouter;
