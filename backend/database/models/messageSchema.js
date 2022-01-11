4a9f058c73cf0cfb0007d50cbd8aa977ebdc0ff2
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-11 02:03:28 +0200

    create message schema

---
+++ backend/database/models/messageSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const messageSchema = new mongoose.Schema(
+  {
+    conversationId: { type: String },
+    sender: { type: String },
+    message: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Posts", messageSchema);
