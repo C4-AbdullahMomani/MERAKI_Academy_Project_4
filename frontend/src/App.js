98157bf07e736fe95b7063a7d297756af010f447
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-12 17:58:13 +0200

    adding welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,15 @@ import React, { useState, createContext, useEffect } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 import "./App.css";
+import Welcome from "./components/welcoming";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome To App</h1>
+     
+      <Routes>
+      <Route path="/" element={ <Welcome/> } />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/welcoming.js
@@ -0,0 +1,17 @@
+import React, { useState, createContext, useEffect } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "./css/welcome.css"
+
+
+function Welcome() {
+  return (
+    <div className="welcome">
+      <h1>Welcome To App</h1>
+    
+    </div>
+  );
+}
+function Navigate(){
+
+}
+export default Welcome;
