b9f675748f9b744bc868a8a0a86f5e115ba369e2
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-13 00:41:13 +0200

    adding register function and css on register and login pages

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,15 +1,19 @@
 import React, { useState, createContext, useEffect } from "react";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import  Navigate from "../components/welcoming" 
 import "./css/login.css"
 
 
 function LogIn() {
   return (<>
     <div className="login">
+      {/* <Navigate/> */}
+      <div style={{height:"175px"}}></div>
      <div>
+     <h2 style={{color:"white",textDecoration:"underLine"}}>Login</h2>
       <input placeholder="EMAIL"/><br/>
       <br/>
-      <input placeholder="Password"/><br/>
+      <input placeholder="Password"/><br/><br/><br/>
     <button>Login</button> </div></div>
      </>
   );
--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,56 +1,117 @@
 import React, { useState, createContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 import "./css/register.css";
 
 function Register() {
   const [firstName, setFirstName] = useState("");
-  const [lasttName, setLastName] = useState("");
-  const [emailName, setEmail] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   const [passWord, setPassWord] = useState("");
   const [country, setCountry] = useState("");
   const [image, setImage] = useState("");
-  console.log(image)
+  const [success, setSuccess] = useState();
+  const [message, setMessage] = useState("");
+  const [role, setRole] = useState("61dcdf48b149561499f8fb85");
+  console.log(image);
+  const navigate=useNavigate()
   return (
     <>
       <div className="Register">
         <div>
+        <div style={{height:"100px"}}></div>
+        <h2 style={{color:"white",textDecoration:"underLine"}}>Register</h2>
+          <input
+            type={"text"}
+            placeholder="First Name"
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
           <br />
           <br />
-          <input type={"text"} placeholder="First Name" onChange={(e) => {
-              setFirstName(e.target.value)
-          }} />
+          
+          <input
+            type={"text"}
+            placeholder="Last Name"
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+          />
           <br />
           <br />
-          <input type={"text"} placeholder="Last Name" onChange={(e) => {
-              setLastName(e.target.value)
-          }} />
+          <input
+            type={"email"}
+            placeholder="EMAIL"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
           <br />
           <br />
-          <input type={"email"} placeholder="EMAIL" onChange={(e) => {
-              setEmail(e.target.value)
-          }}/>
+          <input
+            type={"password"}
+            placeholder="Password"
+            onChange={(e) => {
+              setPassWord(e.target.value);
+            }}
+          />
           <br />
           <br />
-          <input type={"password"} placeholder="Password" onChange={(e) => {
-              setPassWord(e.target.value)
-          }}/>
+          <input
+            type={"text"}
+            placeholder="Country"
+            onChange={(e) => {
+              setCountry(e.target.value);
+            }}
+          />
           <br />
           <br />
-          <input type={"text"} placeholder="Country" onChange={(e) => {
-              setCountry(e.target.value)
-          }}/>
+          <input
+            type={"file"}
+            onChange={(e) => {
+              setImage(e.target.value.split("").splice(12).join(""));
+            }}
+          />
           <br />
           <br />
-          <input type={"file"} onChange={(e) => {
-              setImage(e.target.value.split("").splice(12).join(""))
-              
-          }}/>
-          <br />
-          <br />
-          <button onClick={()=>{
- 
-          }}>Register</button>{" "}
+          <button
+            onClick={() => {
+              setSuccess(true);
+              firstName && lastName && country && email && passWord && role
+                ? axios
+                    .post("http://localhost:5000/users/", {
+                      firstName: firstName,
+                      lastName: lastName,
+
+                      country: country,
+                      email: email,
+                      passWord: passWord,
+                      role: role,
+                    })
+                    .then((response) => {
+                        console.log(response);
+                      setMessage("The user has been created successfully");
+                      setFirstName("");
+                      setLastName("");
+                      setEmail("");
+                      setPassWord("");
+                      setCountry("");
+                      setImage("");
+                      setTimeout(()=>{
+                        navigate("/login")
+                      },3000)
+                      
+                    })
+                    .catch((err) => {
+                      setMessage(err.response.data.message);
+                    })
+                : setMessage("Error");
+            }}
+          >
+            Register
+          </button>{" "}
+          <p>{success ? message : ""}</p>
         </div>
       </div>
     </>
