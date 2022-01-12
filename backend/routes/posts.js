0247052c117c4fe46741d0ca86ebe3d94a13cf03
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-12 17:24:19 +0200

    install react-router-dom as a dependency

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -7,18 +7,22 @@ const {
   updatePostByAuthorId,
   deletePostById,
   createNewComment,
-  updatePostLikes
+  updatePostLikes,
 } = require("../controllers/posts");
 const { post } = require("./conversation");
 
 const authentication = require("../middleware/authentication");
 const authorization = require("../middleware/authorization");
-postRouter.post("/",authentication, createNewPost);
-postRouter.get("/",authentication, getAllPosts);
-postRouter.get("/:id",authentication, getAllPostsByAuthorId);
+postRouter.post("/", authentication, createNewPost);
+postRouter.get("/", authentication, getAllPosts);
+postRouter.get("/:id", authentication, getAllPostsByAuthorId);
 postRouter.put("/:id", updatePostByAuthorId);
 postRouter.delete("/:id", deletePostById);
-postRouter.post("/:id/comments",authentication,
-authorization("CREATE_COMMENTS"),createNewComment)
-postRouter.put("/:id/likes",updatePostLikes)
+postRouter.post(
+  "/:id/comments",
+  authentication,
+  authorization("CREATE_COMMENTS"),
+  createNewComment
+);
+postRouter.put("/:id/likes", updatePostLikes);
 module.exports = postRouter;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,6 @@
+import React, { useState, createContext, useEffect } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
