8ee8d34ffb3a820b67e11eef89352ba4d9434f66
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-13 23:08:53 +0200

    adding comment input and button

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,19 +2,23 @@ import React, { useState, createContext, useEffect } from "react";
 import {  Link, useNavigate } from "react-router-dom";
 import HomePost from "./homePost";
 import "./css/Home.css";
+import axios from "axios";
+import UserInfo from "./userInformation";
 
-function Home() {
+function Home({user,userInfo}) {
   const navigate = useNavigate();
+ const [userImg,setUserImg]=useState(user)
+ 
+ console.log(user);
   return (
     <div className="home">
       {/* <h1 style={{color:"white"}}>hello to our page</h1> */}
+      <div></div>
       <div className="information">
         {" "}
         <div className="top">
-          <img src="https://i.etsystatic.com/22479060/r/il/b8c9e7/2750779710/il_570xN.2750779710_t69p.jpg" />
-          <h3 style={{ textAlign: "center", color: "white", height: "5px" }}>
-            abdallah almomani
-          </h3>
+          <img src="https://i.etsystatic.com/22479060/r/il/b8c9e7/2750779710/il_570xN.2750779710_t69p.jpg" />:<img/>
+          <UserInfo userInfo={userInfo} />
         </div>
         <div>
           <Navigate />
