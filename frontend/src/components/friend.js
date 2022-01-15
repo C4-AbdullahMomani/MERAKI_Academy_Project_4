4cdc490d60121dc2b6426245ffeffa3ca9a7b937
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-16 01:49:33 +0200

    add friend online part

--- frontend/src/components/friend.js
+++ frontend/src/components/friend.js
@@ -1,18 +1,49 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect ,createContext} from "react";
 import "./css/friend.css";
-export default function Friend() {
+
+
+
+
+export default function Friend({ onLineUsers, setCurrentChat }) {
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("userInfo"))
+  );
+  const [friends, setFriends] = useState([]);
+  const [onLineFriends, setOnLineFriends] = useState([]);
+console.log(friends);
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await axios.get("http://localhost:5000/users");
+      try {
+        console.log(res);
+        setFriends(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFriends();
+  }, []);
+
+  useEffect(() => {
+    setOnLineFriends(friends.filter((f) => onLineUsers.includes(f._id)));
+  }, [friends, onLineUsers]);
+
   return (
+    
     <div className="chatOnLine">
-      <div className="chatFriend">
-        <div className="imgContainer">
-          <img
-            className="img"
-            src="https://thumbs.dreamstime.com/b/bobcat-kitten-15843174.jpg"
-          />
+      {onLineFriends.map((o) => (
+        <div className="chatFriend">
+          <div className="imgContainer">
+            <img
+              className="img"
+              src="https://thumbs.dreamstime.com/b/bobcat-kitten-15843174.jpg"
+            />
+          </div>
+          <div className="chatOnlineBadge"></div>
+          <span className="chatOnlineName">{`${o?.firstName} ${o?.lastName}`}</span>
         </div>
-        <div className="chatOnlineBadge"></div>
-        <span className="chatOnlineName">momani</span>
-      </div>
+      ))}
     </div>
   );
 }
