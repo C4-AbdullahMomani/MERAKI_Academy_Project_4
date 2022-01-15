ce199874b5de27799271371fc497045324fca0ee
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-16 01:55:35 +0200

    update user information part

--- frontend/src/components/userInformation.js
+++ frontend/src/components/userInformation.js
@@ -1,11 +1,13 @@
 import React, { useState, createContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import axios from "axios";
 
 
 function UserInfo({userInfo}) {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [isLogIn, setIsLogIn] = useState(localStorage.getItem("isLogIn"));
+  
+  
 
   const[user,setUser]=useState(userInfo)
   console.log(userInfo);
@@ -14,7 +16,7 @@ function UserInfo({userInfo}) {
   console.log(token, isLogIn);
   return (
     <div className="">
-      <div style={{color:"white"}}>{user?`${user.firstName} ${user.lastName}`:""}</div>
+      <div style={{color:"white",marginTop:"15px",marginBottom:"15px"}}>{user?`${user.firstName} ${user.lastName}`:undefined}</div>
     </div>
   );
 }
