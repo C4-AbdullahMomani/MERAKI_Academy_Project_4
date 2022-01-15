13c7297c6915a8bceadf80d031cdfa833d21d8df
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-15 05:03:08 +0200

    add conversation friend

---
+++ frontend/src/components/conversations.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React, { useState, createContext, useEffect } from "react";
+import "./css/conversations.css";
+function Conversations({ conversation }) {
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("userInfo"))
+  );
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const friendId = conversation.members.find(
+      (member) => member !== userInfo._id
+    );
+    setUser(friendId);
+    const getFriend = async () => {
+      const res = await axios.get(
+        `http://localhost:5000/users/search/${friendId}`
+      );
+      try {
+        setUser(res.data.user);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFriend();
+  }, []);
+  return (
+    <div className="conversation">
+      <img
+        className="conversationImg"
+        src="https://wallsdesk.com/wp-content/uploads/2018/03/lynx-Images.jpg"
+      />
+      <span className="conversationName">
+        {user ? `${user.firstName} ${user.lastName}` : ""}
+      </span>
+    </div>
+  );
+}
+
+export default Conversations;
