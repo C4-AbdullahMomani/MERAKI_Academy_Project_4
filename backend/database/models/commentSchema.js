08bf3486ddf1c092bd5d23655daf8486cce516fc
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-15 05:09:26 +0200

    update comment schema to get the time

--- backend/database/models/commentSchema.js
+++ backend/database/models/commentSchema.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose");
 const commentSchema = new mongoose.Schema({
   comment: { type: String, required: true },
   commenter: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
+},{timestamps:true});
 
 module.exports = mongoose.model("Comment", commentSchema);
