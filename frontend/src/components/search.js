da9cba382bfa91098202ee6ba70e31cc3dc0f1e0
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-18 00:15:09 +0200

    add search component

---
+++ frontend/src/components/search.js
@@ -0,0 +1,41 @@
+import React, { useState, createContext, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Markunread } from "@material-ui/icons";
+import "../components/css/search.css"
+function SearchFriend({user}) {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [isLogIn, setIsLogIn] = useState(localStorage.getItem("isLogIn"));
+  const navigate = useNavigate();
+
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("userInfo"))
+  );
+
+  const [friends, setFriends] = useState([]);
+  
+
+  
+
+  return (
+    <div className="">
+      
+            <div className="chatFriend">
+              <div className="imgContainer">
+              {user.image? (<img
+                  className="img"
+                  src={user.image}
+                />):""}
+              </div>
+              
+              <span className="chatOnlineName">
+                {user ? `${user.firstName} ${user.lastName}` : ""}
+              </span>
+            </div>
+
+        
+    </div>
+  );
+}
+
+export default SearchFriend;
