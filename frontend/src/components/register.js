26d6eb92bcbf79add74f8301d800de78cb33ffbc
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-17 19:52:39 +0200

    update regster component to upload image while registering

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -2,7 +2,7 @@ import React, { useState, createContext, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./css/register.css";
-
+import { storage } from "./fireBase";
 function Register() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -15,6 +15,53 @@ function Register() {
   const [role, setRole] = useState("61dcdf48b149561499f8fb85");
   console.log(image);
   const navigate = useNavigate();
+
+  ////firebase
+
+  const allInputs = { imgUrl: "" };
+  const [imageAsFile, setImageAsFile] = useState("");
+  const [imageAsUrl, setImageAsUrl] = useState(allInputs);
+  console.log(imageAsFile);
+  
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0];
+    setImageAsFile((imageFile) => image);
+  };
+  
+  const handleFireBaseUpload = e => {
+    e.preventDefault()
+  console.log('start of upload')
+  // async magic goes here...
+  if(imageAsFile === '') {
+    console.error(`not an image, the image file is a ${typeof(imageAsFile)}`)
+  }
+  const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
+  //initiates the firebase side uploading 
+  uploadTask.on('state_changed', 
+  (snapShot) => {
+    //takes a snap shot of the process as it is happening
+    console.log(snapShot)
+  }, (err) => {
+    //catches the errors
+    console.log(err)
+  }, () => {
+    // gets the functions from storage refences the image storage in firebase by the children
+    // gets the download url then sets the image from firebase as the value for the imgUrl key:
+    storage.ref('images').child(imageAsFile.name).getDownloadURL()
+     .then(fireBaseUrl => {
+       setImageAsUrl(prevObject => ({...prevObject, imgUrl: fireBaseUrl}))
+       console.log(imageAsUrl);
+     })
+  })
+  }
+
+
+
+
+
+
+////////////////
+
   return (
     <>
       <div className="Register">
@@ -68,12 +115,10 @@ function Register() {
           />
           <br />
           <br />
-          <input
-            type={"file"}
-            onChange={(e) => {
-              setImage(e.target.value.split("").splice(12).join(""));
-            }}
-          />
+          <form onSubmit={handleFireBaseUpload}>
+              <input type="file" onChange={handleImageAsFile} />
+              <button>upload</button>
+            </form>
           <br />
           <br />
           <button
@@ -84,7 +129,7 @@ function Register() {
                     .post("http://localhost:5000/users/", {
                       firstName: firstName,
                       lastName: lastName,
-
+                      image:imageAsUrl.imgUrl,
                       country: country,
                       email: email,
                       passWord: passWord,
