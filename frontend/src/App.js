bebe402e1bb1574057becca244876761234d44ac
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-01-19 06:08:07 +0200

    add friendProfile route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import LogIn from "./components/login";
 import Register from "./components/register";
 import Home from "./components/home";
 import Messenger from "./components/messenger";
+import FriendsProfile from "./components/friendsProfile";
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [isLogIn, setIsLogIn] = useState(localStorage.getItem("isLogIn"));
   const [user, setUser] = useState();
-
+  const [profileId,setProfileId]=useState()
+console.log(profileId);
   const [userInfo, setUserInfo] = useState(
     JSON.parse(localStorage.getItem("userInfo"))
   );
@@ -24,8 +26,9 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route
           path="/home"
-          element={<Home user={user} userInfo={userInfo} />}
+          element={<Home user={user} userInfo={userInfo} setProfileId={setProfileId}/>}
         />
+        <Route path="/profile"  element={<FriendsProfile profileId={profileId}/>}/>
         <Route path="/messenger" element={<Messenger />} />
         <Route path="/login" element={<LogIn setUser={setUser} />} />
         <Route path="/users" element={<Register />} />
